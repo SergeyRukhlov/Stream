b48bc1edf4a710f99c8712f47d9d8e86f7adefc0
Sergey Rukhlov <rukhlov-s@mail.ru>
2022-07-10 16:48:40 +0300

    Stream1

---
+++ Stream1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine();
+
+        int size = 20;
+
+        int[] FillArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = new Random().Next(100, 1000);
+            }
+            return array;
+        }
+
+        void PrintResult(int[] array)
+        {
+            int even = 0;
+            int uneven = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+                if (array[i] % 2 == 0) even++;
+                else uneven++;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Количество четных чисел {even}");
+            Console.WriteLine();
+        }
+
+        PrintResult(FillArray(size));
+    }
+}
